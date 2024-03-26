05bff5fe495b44952ba27fc02665ac9a3bafe1f6
august sletto <augustsletto@gmail.com>
2024-03-26 23:15:11 +0000

    deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,27 @@
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-import HomeScreen from './screens/HomeScreen'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Switch>
+            <Route path='/' component={HomeScreen} exact />
+            <Route path='/product/:id' component={ProductScreen} />
+          </Switch>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import { Navbar, Nav, Container, Row } from 'react-bootstrap'
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
 
 function Header() {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">BuySmart</Navbar.Brand>
+                    <NavLink to='/' className="navbar-brand">
+                        BuySmart
+                    </NavLink>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-                            <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
-
+                            <NavLink to='/cart' className="nav-link">
+                                <i className='fas fa-shopping-cart'></i>Cart
+                            </NavLink>
+                            <NavLink to='/login' className="nav-link">
+                                <i className='fas fa-user'></i>Login
+                            </NavLink>
                         </Nav>
-
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
