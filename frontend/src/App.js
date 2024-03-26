6aa6144b8191273bce696ad5101a19a0b1705a8c
august sletto <augustsletto@gmail.com>
2024-03-26 11:40:28 +0000

    Initial commit, react app started

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>
+          Welcome
+        </h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
