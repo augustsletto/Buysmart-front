a52fd40f6fdae170aba397614273ae2b36cd264c
august sletto <augustsletto@gmail.com>
2024-03-27 16:52:41 +0000

    Cart locally stored functionality  added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 import "./api/axiosDefaults"
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Switch>
             <Route path='/' component={HomeScreen} exact />
             <Route path='/product/:id' component={ProductScreen} />
+            <Route path='/cart/:id?' component={CartScreen} />
           </Switch>
         </Container>
       </main>
