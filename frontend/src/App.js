150620df919a917e3d55bd931539420c6d15af8c
august sletto <augustsletto@gmail.com>
2024-03-27 01:24:06 +0000

    axios ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Footer from './components/Footer';
 
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-
+import "./api/axiosDefaults"
 
 function App() {
   return (
