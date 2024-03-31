5b0024029b6a3115a5aae8cef959a55f6bb08aad
august sletto <augustsletto@gmail.com>
2024-03-31 12:16:30 +0000

    dpeploy fix

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 import {
     productListReducers,
